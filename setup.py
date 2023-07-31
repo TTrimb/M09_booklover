d031bbe27cce9a9541d2718a47b460837d3e7d38
TTrimb <59318718+TTrimb@users.noreply.github.com>
2023-07-31 12:50:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup, find_packages
 
+setup(name='Booklover',
+      version='0.1',
+      description='Books for book lover',
+      url='https://github.com/TTrimb/M09_booklover/tree/main/demo',
+      author='Taryn',
+      author_email='hsh5rs@virginia.edu',
+      license='MIT',
+      packages= find_packages())
